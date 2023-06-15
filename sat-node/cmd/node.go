b28144c3ab21eac73bc34376635561301cbb84aa
qiutb <Hikariion@foxmail.com>
2023-06-15 15:54:06 +0800

    (chore): add some code

--- sat-node/cmd/node.go
+++ sat-node/cmd/node.go
@@ -1,1 +1,25 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var nodeCmd = &cobra.Command{
+	Use:   "node {run}",
+	Short: "satellite node operate",
+}
+
+var nodeRunCmd = &cobra.Command{
+	Use:   "run",
+	Short: "start run satellite node",
+}
+
+func init() {
+	nodeRunCmd.Flags().StringP("sunAddr", "s", "", "ground station addr")
+}
+
+//func nodeRun(cmd *cobra.Command, _ []string)  {
+//	// read config
+//	confPath := cmd.Flag("config").Value.String()
+//	conf := config.DefaultConfig
+//}
