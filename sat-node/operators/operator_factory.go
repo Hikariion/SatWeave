a23037ce8a2f49423945ba9b0c3a694cf7cd3b37
Hikariion <Hikariion@foxmail.com>
2024-03-30 21:48:05 +0800

    update

--- sat-node/operators/operator_factory.go
+++ sat-node/operators/operator_factory.go
@@ -3,12 +3,13 @@ package operators
 type OperatorFactory func() OperatorBase
 
 var FactoryMap = map[string]OperatorFactory{
-	"SimpleSource": func() OperatorBase { return new(SimpleSource) },
-	"SimpleSink":   func() OperatorBase { return new(SimpleSink) },
-	"KeyByInputOp": func() OperatorBase { return new(KeyByInputOp) },
-	"FFTOp":        func() OperatorBase { return new(FFTOp) },
-	"HaFilterOp":   func() OperatorBase { return new(HaFilterOp) },
-	"HaSumOp":      func() OperatorBase { return new(HaSumOp) },
-	"LaFilterOp":   func() OperatorBase { return new(LaFilterOp) },
-	"LaSumOp":      func() OperatorBase { return new(LaSumOp) },
+	"SimpleSource":    func() OperatorBase { return new(SimpleSource) },
+	"SimpleSink":      func() OperatorBase { return new(SimpleSink) },
+	"KeyByInputOp":    func() OperatorBase { return new(KeyByInputOp) },
+	"FFTOp":           func() OperatorBase { return new(FFTOp) },
+	"HaFilterOp":      func() OperatorBase { return new(HaFilterOp) },
+	"HaSumOp":         func() OperatorBase { return new(HaSumOp) },
+	"LaFilterOp":      func() OperatorBase { return new(LaFilterOp) },
+	"LaSumOp":         func() OperatorBase { return new(LaSumOp) },
+	"SimpleFFTSource": func() OperatorBase { return new(SimpleFFTSource) },
 }
